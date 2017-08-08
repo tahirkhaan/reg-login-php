c96de47f08e62ce894a7077958c86802d002d28c
tahirkhaan <tahirkhan.dream@gmail.com>
2017-08-08 23:22:50 +0500

    start how to make a registration and login system in php

---
+++ register.php
@@ -0,0 +1,48 @@
+<?php
+
+
+
+?>
+
+
+
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>
+		Registration Form
+	</title>
+</head>
+<body>
+<div class="header">
+	<h1>Registration Heres</h1>
+</div>
+
+<form method="post" action="register.php">
+	<table>
+		<tr>
+			<td>UserName:</td>
+			<td><input type="text" name="username" class="textinput"></td>
+		</tr>
+		<tr>
+			<td>Email:</td>
+			<td><input type="text" name="username" class="textinput"></td>
+		</tr>
+		<tr>
+			<td>Password:</td>
+			<td><input type="password" name="password" class="textinput"></td>
+		</tr>
+		<tr>
+			<td>Confirm Password:</td>
+			<td><input type="password" name="password2" class="textinput"></td>
+		</tr>
+		<tr>
+			<td><input type="submit" name="register_btn" value="Register"></td>
+		</tr>
+	</table>
+</form>
+
+</body>
+</html>
